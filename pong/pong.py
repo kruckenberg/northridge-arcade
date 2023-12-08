f457090edc157f3624422ad52d776c90fcc09291
Nicholas Kruckenberg <nkruckenberg@gmail.com>
2023-12-08 02:44:09 -0600

    check for and handle collisions, points

--- pong/pong.py
+++ pong/pong.py
@@ -15,9 +15,9 @@ def play_pong():
     midpoint = 0
 
     # ball globals
-    ball_x_speed = 0.15
+    ball_x_speed = 0.1
     ball_x_direction = 1
-    ball_y_speed = 0.15
+    ball_y_speed = 0.1
     ball_y_direction = 1
 
     # score
@@ -62,6 +62,14 @@ def play_pong():
         ball.setx(ball.xcor() + (ball_x_speed * ball_x_direction))
         ball.sety(ball.ycor() + (ball_y_speed * ball_y_direction))
 
+    def change_ball_y_direction():
+        nonlocal ball_y_direction
+        ball_y_direction *= -1
+
+    def change_ball_x_direction():
+        nonlocal ball_x_direction
+        ball_x_direction *= -1
+
     # paddle movement
     def move_l_paddle_up():
         l_paddle.sety(l_paddle.ycor() + 20)
@@ -81,11 +89,69 @@ def play_pong():
     screen.onkeypress(move_r_paddle_up, "Up")
     screen.onkeypress(move_r_paddle_down, "Down")
 
+    # check for wall collisions
+    def check_for_wall_collision():
+        ball_y_position = ball.ycor()
+
+        if ball_y_position > top_edge or ball_y_position < bottom_edge:
+            change_ball_y_direction()
+
+    def check_for_l_paddle_collision():
+        ball_x_position, ball_y_position = ball.position()
+        l_paddle_x_position, l_paddle_y_position = l_paddle.position()
+
+        # determine if ball is within paddle's y range
+        if (
+            ball_y_position >= l_paddle_y_position - 50
+            and ball_y_position <= l_paddle_y_position + 50
+        ):
+            # determine if ball is within paddle's x range
+            if (
+                ball_x_position <= l_paddle_x_position + 20
+                and ball_x_position >= l_paddle_x_position - 20
+            ):
+                change_ball_x_direction()
+
+    def check_for_r_paddle_collision():
+        ball_x_position, ball_y_position = ball.position()
+        r_paddle_x_position, r_paddle_y_position = r_paddle.position()
+
+        # determine if ball is within paddle's y range
+        if (
+            ball_y_position >= r_paddle_y_position - 50
+            and ball_y_position <= r_paddle_y_position + 50
+        ):
+            # determine if ball is within paddle's x range
+            if (
+                ball_x_position <= r_paddle_x_position + 20
+                and ball_x_position >= r_paddle_x_position - 20
+            ):
+                change_ball_x_direction()
+
+    def check_for_point():
+        ball_x_position, ball_y_position = ball.position()
+
+        if ball_x_position < left_edge:
+            nonlocal score_right
+            score_right += 1
+            change_ball_x_direction()
+            ball.goto(midpoint, midpoint)
+
+        if ball_x_position > right_edge:
+            nonlocal score_left
+            score_left += 1
+            change_ball_x_direction()
+            ball.goto(midpoint, midpoint)
+
     # main game loop
     while True:
         screen.update()
 
         move_ball()
+        check_for_point()
+        check_for_wall_collision()
+        check_for_l_paddle_collision()
+        check_for_r_paddle_collision()
 
 
 if __name__ == "__main__":
